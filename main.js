9e0b8ac5c3a1947d339b7ddfcae7cc64430ef5cd
NioceDesu <nioce147@gmail.com>
2020-01-05 15:34:45 +0100

    Bottom styling finished

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 250,
         height: 325,
-        // frame: false,
+        frame: false,
+        transparent: true,
         webPreferences: {
             preload: path.join(__dirname, 'nodeIntegrations/preload.js')
             // nodeIntegration: true
--- nodeIntegrations/preload.js
+++ nodeIntegrations/preload.js
@@ -1,4 +1,35 @@
 window.addEventListener('DOMContentLoaded', () => {
     let rr = require('tinycolor2');
     // let dd = require('robotjs');
+    // 
+    const colors = {
+        foreground: "#E7C296",
+        background: "#29344b"
+    }
+    //
+    changeSVGColors(colors);
+    // 
 });
+// Change root colors
+function changeColor(colors) {
+    for (let index = 0; index < Object.keys(colors).length; index++) {
+        document.documentElement.style.setProperty(`--${Object.keys(colors)[index]}`, colors[Object.keys(colors)[index]]);
+    }
+}
+// Get svg color Filter from HEX
+function getSVGFilter(colors) {
+    const svgColor = require('../app/js/svgColor');
+    let svgFilters = {
+        foregroundFilter: "",
+        backgroundFilter: ""
+    }
+    for (let index = 0; index < Object.keys(colors).length; index++) {
+        svgFilters[Object.keys(svgFilters)[index]] = svgColor.execute(colors[Object.keys(colors)[index]]).filter;
+    }
+    return svgFilters;
+}
+// function SVG icons color
+function changeSVGColors(colors) {
+    filter = getSVGFilter(colors);
+    changeColor(filter);
+}
